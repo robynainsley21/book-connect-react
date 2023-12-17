7f92d6ddcd36c5239e8284b44986b4b47b699dc3
robynainsley21 <robynainsley2i@gmail.com>
2023-12-17 15:15:56 +0200

    Fix modal bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,17 +9,6 @@ import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
 import Typography from "@mui/material/Typography";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 
 const Preview = (props) => {
   const { handleClose, chosenBook } = props;
@@ -35,22 +24,26 @@ const Preview = (props) => {
         <button onClick={handleClose}>Close</button>
       </div>
     </Modal> */}
-      <Modal
+      <div key={chosenBook.id}
+        className="preview-overlay"
         open={true}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
+        <Box>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <button onClick={handleClose}>Close</button>
+          </div>
+            <p>Title: {chosenBook.title}</p>
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2 }}>
             Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
           </Typography>
+
         </Box>
-        <button onClick={handleClose}>Close</button>
-      </Modal>
+      </div>
     </>
   );
 };
@@ -76,8 +69,8 @@ const HomePage = () => {
     };
 
     return (
-      <>
-        <button className="book-button" key={id} onClick={openPreview}>
+      <div key={id}>
+        <button className="book-button"  onClick={openPreview}>
           <div className="book-image">
             <img style={{ width: "5rem" }} src={image} alt="book-image" />
           </div>
@@ -91,7 +84,7 @@ const HomePage = () => {
         {selectedBook && openBook && (
           <Preview handleClose={closePreview} chosenBook={book} />
         )}
-      </>
+      </div>
     );
   });
 
@@ -99,7 +92,7 @@ const HomePage = () => {
     <>
       <div className="homePage">
         {eachBook ? (
-          <div className="books-container">{eachBook}</div>
+          <div className="books-container" key={eachBook.id}>{eachBook}</div>
         ) : (
           <Backdrop
             sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
